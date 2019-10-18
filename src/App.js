6042a1fc0e8a41989b7e1c79ffaa0e602506ccdf
onyazuka <onizuka2008@yandex.ru>
2019-10-18 15:23:29 +0300

    change

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Choicer from './examples/Choicer';
-
-function App() {
-  return <Choicer></Choicer>
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Menu from './Menu/Menu';
+import createMenu from './Menu/MenuFactory';
+import * as MenuAnimations from './Menu/MenuAnimations';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export {
+  createMenu,
+  MenuAnimations
+};
+export default Menu;
